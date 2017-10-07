bd980e3d54d5f80c3a2eb2dd8eb8c580f156d660
Sergio Crisostomo <sergiosbox@gmail.com>
2017-10-07 10:30:45 +0200

    add ajax

--- app/app.vue
+++ app/app.vue
@@ -5,6 +5,7 @@
         position: relative;
         border-radius: 4px;
         overflow: hidden;
+        height: 100%;
     }
     .layout-header{
         padding: 20px;
@@ -30,6 +31,7 @@
         color: #9ba7b5;
     }
     .layout-body {
+        height: 100%;
         padding: 20px;
         min-height: 200px;
     }
@@ -42,36 +44,60 @@
             </div>
         </div>
         <div class="layout-header">
-            Add a folder to read here:
-            <Input v-model="inputPath" style="width: 300px; margin-left: 10px;"></Input>
-            <Button type="ghost" @click="addPath" icon="plus"></Button>
         </div>
         <div class="layout-body">
-            <p v-for="path in paths">
-                <Button type="ghost" @click="removePath(path)" icon="minus"></Button>
-                {{path}}
-            </p>
-        </div>
-        <div class="layout-copy">
-            File organizer
+            <tabs value="name1">
+                <tab-pane label="Add folders">
+                    <div>
+                        Add a folder to read here:
+                        <Input v-model="inputPath" style="width: 300px; margin-left: 10px;"></Input>
+                        <Button type="ghost" @click="togglePath(true)" icon="plus"></Button>
+                    </div>
+                    <hr style="margin: 10px 0;">
+                    <p v-for="path in paths">
+                        <Button size="small" type="ghost" @click="togglePath(false)" icon="minus"></Button>
+                        <span class="folder-name">{{path}}</span>
+                        <span class="file-count">({{fileCount(path)}} files)</span>
+                    </p>
+                </tab-pane>
+                <tab-pane label="See files">Maybe a Tree component with the files?</tab-pane>
+                <tab-pane label="Take actions">Something smart</tab-pane>
+            </tabs>
         </div>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
 	el: '#app',
 	data() {
 		return {
-			inputPath: 'home/file-organizer',
-			paths: []
+			inputPath: '/Users/Desktop',
+			paths: [],
+			folders: {}
 		};
 	},
 	watch: {
-		paths() {}
+		paths() {
+			if (this.paths.length === 0) this.folders = {};
+			axios
+				.post('/ajax', {paths: this.paths})
+				.then(res => (this.folders = res.data))
+				.catch(e => console.log(e));
+		}
 	},
 	methods: {
-		addPath() {
-			this.paths.push(this.inputPath);
+		togglePath(add) {
+			if (!this.inputPath.trim()) return;
+			if (add) {
+				if (this.paths.includes(this.inputPath)) return;
+				this.paths = this.paths.concat(this.inputPath);
+			} else {
+				this.paths = this.paths.filter(path => path != this.inputPath);
+			}
+		},
+		fileCount(path) {
+			return this.folders[path] ? this.folders[path].files.length : 0;
 		}
 	}
 };
