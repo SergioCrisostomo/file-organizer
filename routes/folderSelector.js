d6135eaaf214a46b22f2a3ac9332b7a869644c61
Sergio Crisostomo <sergiosbox@gmail.com>
2020-05-21 03:25:50 +0200

    Prettier formating

--- routes/folderSelector.js
+++ routes/folderSelector.js
@@ -1,22 +1,22 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 function isDirectory(contentPath) {
-	return fs.statSync(contentPath).isDirectory();
+  return fs.statSync(contentPath).isDirectory();
 }
 
 module.exports = (req, res) => {
-	const folderPath = req.body.path;
-	fs.readdir(folderPath, (err, contents) => {
-		if (err) return console.log(err);
-		contents = contents.filter(item => !/(^|\/)\.[^\/\.]/g.test(item)); // hide hidden folders and junk
-		const subFolders = [];
-		const files = [];
-		for (let subpath of contents) {
-			const contentPath = path.join(folderPath, subpath);
-			if (isDirectory(contentPath)) subFolders.push(contentPath);
-			else files.push(contentPath);
-		}
-		res.send({ subFolders, files });
-	});
+  const folderPath = req.body.path;
+  fs.readdir(folderPath, (err, contents) => {
+    if (err) return console.log(err);
+    contents = contents.filter((item) => !/(^|\/)\.[^\/\.]/g.test(item)); // hide hidden folders and junk
+    const subFolders = [];
+    const files = [];
+    for (let subpath of contents) {
+      const contentPath = path.join(folderPath, subpath);
+      if (isDirectory(contentPath)) subFolders.push(contentPath);
+      else files.push(contentPath);
+    }
+    res.send({ subFolders, files });
+  });
 };
--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const organizer = require('./routes/organizer');
-const folderSelector = require('./routes/folderSelector');
-const checkFolderExists = require('./routes/checkFolderExists');
+const express = require("express");
+const bodyParser = require("body-parser");
+const organizer = require("./routes/organizer");
+const folderSelector = require("./routes/folderSelector");
+const checkFolderExists = require("./routes/checkFolderExists");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + '/localhost'));
+app.use(express.static(__dirname + "/localhost"));
 
-app.post('/organizer', organizer);
-app.post('/folder-selector', folderSelector);
-app.post('/check-path', checkFolderExists);
+app.post("/organizer", organizer);
+app.post("/folder-selector", folderSelector);
+app.post("/check-path", checkFolderExists);
 
-const fs = require('fs');
-app.get('/iview.js', (req, res) => {
-	const file = fs.readFileSync('../iview/dist/iview.js', 'utf8');
-	res.send(file);
+const fs = require("fs");
+app.get("/iview.js", (req, res) => {
+  const file = fs.readFileSync("../iview/dist/iview.js", "utf8");
+  res.send(file);
 });
 
-app.use(function(err, req, res, next) {
-	const status = err.status || 500;
-	res.status(status);
-	res.send(err);
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  res.status(status);
+  res.send(err);
 });
 
-const server = app.listen(process.env.PORT || 8080, function() {
-	const host = server.address().address;
-	const port = server.address().port;
-	console.log('App listening at http://%s:%s', host, port);
+const server = app.listen(process.env.PORT || 8080, function () {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("App listening at http://%s:%s", host, port);
 });
