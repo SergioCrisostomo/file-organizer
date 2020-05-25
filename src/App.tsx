b1543e1443a38ad51df459d1e0408699f09c8d36
SergioCrisostomo <sergiosbox@gmail.com>
2020-05-25 14:45:26 +0200

    Add first Tree view

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,102 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import store, { updateNode } from "./store";
 
-function App() {
+import "./App.css";
+import { TreeNodeType } from "./reducers/types";
+import fetchPath from "./utils/fetchPath";
+
+interface Props {}
+interface State {
+  folderTree: TreeNodeType;
+}
+
+const { getState, subscribe } = store;
+
+const FileNode = (name) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div key={name} className="app-selection-node">
+      <input type="checkbox" />
+      {name}
     </div>
   );
+};
+
+const ExpandableNode = (props) => {
+  const dirname = props.path.split("/").pop();
+
+  const onIconClick = async () => {
+    const expanded = !props.expanded;
+    const nodeDate = await fetchPath(props.path);
+    store.dispatch(
+      updateNode({
+        path: props.path,
+        checked: false,
+        expanded: expanded,
+        subFolders: nodeDate.subFolders.map((str) => ({ path: str })),
+        files: nodeDate.files,
+      })
+    );
+  };
+
+  return (
+    <div key={props.path} className="app-selection-node">
+      <input type="checkbox" />
+      /{dirname}
+      <i onClick={onIconClick}>📂</i>
+      {props.expanded && TreeNode({ props })}
+    </div>
+  );
+};
+
+const TreeNode = ({ props }) => {
+  const getFiles = (props) => {
+    const files = props.files || [];
+    return files.map(FileNode);
+  };
+
+  const getSubFolders = (props) => {
+    const subFolders = props.subFolders || [];
+    return subFolders.map(ExpandableNode);
+  };
+
+  const subfolders = getSubFolders(props);
+  const files = getFiles(props);
+
+  return (
+    <div className="app-tree-node" key={props.path}>
+      <div>{subfolders}</div>
+      <div>{files}</div>
+    </div>
+  );
+};
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      folderTree: { path: "/", checked: false, subFolders: [], files: [] },
+    };
+  }
+
+  componentDidMount() {
+    subscribe(() => {
+      console.log("Subscriber called");
+      this.setState({
+        folderTree: getState().folderTree,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="file-organiser-app">
+        <header className="app-header">
+          <h1>File organizer</h1>
+        </header>
+        <TreeNode props={this.state.folderTree} />
+      </div>
+    );
+  }
 }
 
 export default App;
