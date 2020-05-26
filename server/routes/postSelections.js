8b750f0b161c20941af04c9914ae06539823d795
SergioCrisostomo <sergiosbox@gmail.com>
2020-05-26 16:34:42 +0200

    Use recursive file getter (getFilesRecursively)

--- server/routes/postSelections.js
+++ server/routes/postSelections.js
@@ -1,8 +1,25 @@
-const fs = require("fs");
-const path = require("path");
+const iterator = require("../utils/getFilesRecursively");
+
+const getFilesFromSelection = (arr, types = []) =>
+  arr.reduce((promises, entry) => {
+    const typesPromises = types.map((type) => iterator(entry, type));
+    return [...promises, ...typesPromises];
+  }, []);
 
 module.exports = (req, res) => {
-  const selections = req.body;
+  console.log("Processing...", req.body);
 
-  res.send(selections);
+  const { files, folders } = req.body;
+  const promises = getFilesFromSelection([...files, ...folders], [".jpg"]);
+
+
+  Promise.all(promises)
+    .then((allFiles) => {
+      const files = allFiles.flat
+      res.send(allFiles.flat())
+    })
+    .catch((err) => {
+      console.log("ERR", err);
+      res.send(err);
+    });
 };
