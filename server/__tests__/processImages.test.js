007807cd34b6ec802a54a7eb768b7f11d1a2f724
SergioCrisostomo <sergiosbox@gmail.com>
2020-05-27 21:44:28 +0200

    Add stats test

--- server/__tests__/processImages.test.js
+++ server/__tests__/processImages.test.js
@@ -1,13 +1,17 @@
 const path = require("path");
 const getFilesFromSelection = require("../utils/getFilesFromSelection");
 const filesAnalyser = require("../../lib/index");
-
 const testImagesFolder = path.join(__dirname, "/images");
+const projectRoot = path.join(__dirname, "../../../");
 
 const getTestFiles = async () => {
   const fetchers = getFilesFromSelection([testImagesFolder], [".jpg"]);
   const paths = await Promise.all(fetchers);
   return paths.flat();
+};
+
+const anonymizeFolders = (str) => {
+  return str.split(projectRoot).join("");
 };
 
 test("Detect correct amount of images", async () => {
@@ -20,14 +24,38 @@ test("Generate a report", async () => {
   const report = await filesAnalyser(files);
   const expectedReport = [
     [
-      "/Users/sergiocrisostomo/github/file-organizer/server/__tests__/images/folder_a/bairro_alto-elevador_da_bica.jpg",
-      "/Users/sergiocrisostomo/github/file-organizer/server/__tests__/images/folder_b/bairro_alto-elevador_da_bica.jpg",
-      "/Users/sergiocrisostomo/github/file-organizer/server/__tests__/images/folder_b/bairro_alto-elevador_da_bica0.jpg",
+      "file-organizer/server/__tests__/images/folder_a/bairro_alto-elevador_da_bica.jpg",
+      "file-organizer/server/__tests__/images/folder_b/bairro_alto-elevador_da_bica.jpg",
+      "file-organizer/server/__tests__/images/folder_b/bairro_alto-elevador_da_bica0.jpg",
     ],
     [
-      "/Users/sergiocrisostomo/github/file-organizer/server/__tests__/images/folder_a/beach.jpg",
-      "/Users/sergiocrisostomo/github/file-organizer/server/__tests__/images/folder_b/beach.jpg",
+      "file-organizer/server/__tests__/images/folder_a/beach.jpg",
+      "file-organizer/server/__tests__/images/folder_b/beach.jpg",
     ],
   ];
-  expect(report).toEqual(expectedReport);
+  const input = anonymizeFolders(JSON.stringify(report));
+  const output = anonymizeFolders(JSON.stringify(expectedReport));
+  expect(input).toEqual(output);
+});
+
+test("Should get file stats", async () => {
+  const testFile = path.join(
+    projectRoot,
+    "/file-organizer/server/__tests__/images/folder_a/bairro_alto-elevador_da_bica.jpg"
+  );
+  const getFileStats = require("../../server/routes/getFileStats");
+  let response = null;
+  await getFileStats(
+    { query: { file: testFile } },
+    {
+      send(data) {
+        response = data;
+      },
+    }
+  );
+
+  // remove access timestamps, not relevant to the test...
+  ["atime", "atimeMs"].forEach((key) => delete response.stats[key]);
+
+  expect(response).toMatchSnapshot();
 });
